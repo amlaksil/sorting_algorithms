03683d6c2251c2208fcc82838c75113f4853278f
amlaksil <silamlakdesye@gmail.com>
2023-04-25 09:18:22 +0300

    Fix bug

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -22,11 +22,7 @@ void selection_sort(int *array, size_t size)
 		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[min] < array[j])
-			{
-				continue;
-			}
-			else
+			if (array[j] < array[min])
 			{
 				min = j;
 			}
